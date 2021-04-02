faf1fbbc664c1f6adb24b6295a449069b9502cea
mislavivancic <mislavivancic1@gmail.com>
2021-04-02 10:36:38 +0200

    small refactor

--- app/src/main/java/com/mtkreader/views/fragments/ConnectView.kt
+++ app/src/main/java/com/mtkreader/views/fragments/ConnectView.kt
@@ -22,6 +22,7 @@ import com.mtkreader.utils.SharedPrefsUtils
 import com.mtkreader.views.adapters.ConnectedDevicesRecyclerView
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
 import kotlinx.android.synthetic.main.fragment_connect.*
+import net.alexandroid.utils.mylogkt.logD
 
 class ConnectView : BaseMVPFragment<ConnectionContract.Presenter>(), ConnectionContract.View,
     ConnectedDevicesRecyclerView.OnItemClickListener {
